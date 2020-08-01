a9e868a34298f9b07040c66952d441d111a72a70
Jucni Bie <jucnibie.meow@gmail.com>
2020-08-01 20:41:45 +0700

    third commit

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, quiz
+
 
 app_name = 'vocabulary'
 
 urlpatterns = [
     path('', HomePage.as_view(), name='home'),
+    path('quiz', quiz, name='quiz'),
 ]
--- vocabulary/views.py
+++ vocabulary/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.views import View
+from django.urls import reverse
+
 from .models import Vocabulary
 import random
 
@@ -11,3 +14,10 @@ class HomePage(View):
         vocabularys = list(Vocabulary.objects.all())
         random.shuffle(vocabularys)
         return render(request, 'home.html', {'vocabularys' : vocabularys})
+
+
+def quiz(request):
+    vocabularys = list(Vocabulary.objects.all())
+    random.shuffle(vocabularys)
+    quizlist = list(vocabularys[:10])
+    return render(request, 'quiz.html', {'vocabularys' : quizlist})

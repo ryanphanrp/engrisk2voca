e1b7bb8e0c3d66a6e3e7ce4fe34dcd0bff5e247e
Jucni Bie <jucnibie.meow@gmail.com>
2020-08-17 14:55:55 +0700

    add login required

--- vocabulary/views.py
+++ vocabulary/views.py
@@ -2,20 +2,23 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Vocabulary
 import random
 
 
 # Create your views here.
-class HomePage(View):
+class HomePage(LoginRequiredMixin, View):
+    login_url = '/admin/login/'
 
     def get(self, request):
         vocabularys = list(Vocabulary.objects.all())
         random.shuffle(vocabularys)
         return render(request, 'home.html', {'vocabularys' : vocabularys})
 
-
+@login_required(login_url="/admin/login/")
 def quiz(request):
     vocabularys = list(Vocabulary.objects.all())
     random.shuffle(vocabularys)
